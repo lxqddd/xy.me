23dac9f983ae0c2557844ec21eeab8a9fc0884c4
向阳 <xiangqian_liu@163.com>
2022-07-28 17:46:42 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Markdown from 'vite-plugin-vue-markdown'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    Vue({
+    include: [/\.vue$/, /\.md$/],
+  }),
+  Markdown()]
+})
