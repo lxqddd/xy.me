87dafdaa172098a7f77228f6a0667f059d5e5406
向阳 <xiangqian_liu@163.com>
2022-08-21 22:55:47 +0800

    docs: format

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import { createApp } from 'vue'
+import { createHead } from '@vueuse/head'
+import { route } from './router'
 import './styles/reset.css'
 import './styles/main.scss'
 import './styles/markdown.scss'
-import { createHead } from '@vueuse/head'
-import { route } from './router'
-
 import App from './App.vue'
-// import VueInMd from './components/VueInMd.vue'
 
 const app = createApp(App)
 const head = createHead()
-
-// app.component('VueInMd', VueInMd)
-
 app.use(head)
 app.use(route)
 app.mount('#app')
