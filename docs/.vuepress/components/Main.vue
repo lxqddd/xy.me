46991d90dd68b4daab4a7335126accf085e5fe62
向阳 <xiangqian_liu@163.com>
2022-09-23 09:54:51 +0800

    feat: 调整主页样式

--- docs/.vuepress/components/Main.vue
+++ docs/.vuepress/components/Main.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="main">
-    乾坤已定，你我皆是牛马!
+    <div class="content">
+      乾坤已定，你我皆是牛马!
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
   .main {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    width: 100%;
-    text-align: center;
-    transform: translate(-50%);
-    font-size: 1.95rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     font-family: cursive;
+    font-size: 1.95rem;
+    position: absolute;
+    top: var(--navbar-height);
+    right: 0px;
+    left: 0px;
+    bottom: 0px;
+    .content {
+      margin-top: - var(--navbar-height);
+    }
   }
 </style>
